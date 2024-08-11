4f34f3a14726e012df2c73516461c605e620a7b6
Hammad Majid <hammadmajid@proton.me>
2024-08-12 03:48:05 +0500

    Add middleware to our app & protect against csrf

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import { fromHono } from "chanfana";
 import { Hono } from "hono";
+import { csrf } from "hono/csrf";
 
 import { SignUp } from "endpoints/auth/signup";
 import { SignIn } from "endpoints/auth/signin";
 
 import { Bindings } from "./bindings";
+import { authMiddleware } from "middleware";
 
 // Start a Hono app
 const app = new Hono<{ Bindings: Bindings }>();
+
+// Middleware configuratio
+app.use(csrf());
+app.use("*", authMiddleware);
 
 // Setup OpenAPI registry
 const openapi = fromHono(app, {
