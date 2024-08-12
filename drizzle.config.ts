f7eee04311139045543e87296ca56db025e799d7
Hammad Majid <hammadmajid@proton.me>
2024-08-12 18:44:17 +0500

    Move API code to server/ dir
    Turning this into a monorepo

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   dialect: "sqlite",
-  schema: "./src/db/schema.ts",
+  schema: "./server/db/schema.ts",
   out: "./drizzle",
 });
