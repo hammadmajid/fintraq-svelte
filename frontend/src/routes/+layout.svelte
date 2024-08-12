6626aba9373cd1a53b9a4b375557e5b4ab5f6b4a
Hammad Majid <hammadmajid@proton.me>
2024-08-12 19:24:21 +0500

    Add AppBar to entire app

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,1 +1,16 @@
-<script>import "../app.css";</script><slot></slot>
+<script>
+    import { AppBar } from "@skeletonlabs/skeleton";
+    import "../app.css";
+</script>
+
+<AppBar shadow="drop-shadow-xl" label="header">
+    <svelte:fragment slot="lead"
+        ><p class="h3"><a href="/">Fintraq</a></p></svelte:fragment
+    >
+    <svelte:fragment slot="trail"
+        ><a href="https://github.com/hammadmajid/fintraq"
+            ><i class="w-6 h-6 fa-brands fa-github"></i></a
+        ></svelte:fragment
+    >
+</AppBar>
+<slot></slot>
