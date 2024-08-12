71c776dcef31587e9786841e157a09a157d712aa
Hammad Majid <hammadmajid@proton.me>
2024-08-12 19:23:52 +0500

    Add hero section to homepage

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container p-8 mx-auto space-y-4">
+	<h1 class="h1">Fintraq</h1>
+	<p>A bare minimun finance tracking app built with SvelteKit, Skeleton, and Supabase.</p>
+	<section>
+		<a class="space-x-2 btn variant-filled-primary" href="/auth/signup"
+			><span>Get started</span><i class="fa-solid fa-arrow-right"></i></a
+		>
+		<a class="space-x-2 btn variant-filled-ghost" href="https://github.com/hammadmajid/fintraq"
+			><i class="fa-brands fa-github"></i><span>GitHub</span>
+		</a>
+	</section>
+</div>
