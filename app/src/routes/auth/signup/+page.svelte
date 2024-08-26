8b755251c17ae23b73dc3f3f7c7010170ff70056
Hammad Majid <hammadmajid@proton.me>
2024-08-26 18:00:14 +0500

    create UI of auth routes

---
+++ app/src/routes/auth/signup/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button/index.js";
+  import * as Card from "$lib/components/ui/card/index";
+  import { Input } from "$lib/components/ui/input/index";
+  import { Label } from "$lib/components/ui/label/index";
+</script>
+
+<main class="bg-muted/40 flex min-h-screen w-full flex-col">
+  <div class="my-4 md:my-8"></div>
+  <Card.Root class="mx-auto max-w-sm">
+    <Card.Header>
+      <Card.Title class="text-2xl">Sign Up</Card.Title>
+      <Card.Description
+        >Enter your information to create an account</Card.Description
+      >
+    </Card.Header>
+    <Card.Content>
+      <div class="grid gap-4">
+        <div class="grid grid-cols-2 gap-4">
+          <div class="grid gap-2">
+            <Label for="first-name">First name</Label>
+            <Input id="first-name" placeholder="Max" required />
+          </div>
+          <div class="grid gap-2">
+            <Label for="last-name">Last name</Label>
+            <Input id="last-name" placeholder="Robinson" required />
+          </div>
+        </div>
+        <div class="grid gap-2">
+          <Label for="email">Email</Label>
+          <Input id="email" type="email" placeholder="m@example.com" required />
+        </div>
+        <div class="grid gap-2">
+          <Label for="password">Password</Label>
+          <Input id="password" type="password" />
+        </div>
+        <Button type="submit" class="w-full">Create an account</Button>
+      </div>
+      <div class="mt-4 text-center text-sm">
+        Already have an account?
+        <a href="/auth/signin" class="underline"> Sign in </a>
+      </div>
+    </Card.Content>
+  </Card.Root>
+</main>
