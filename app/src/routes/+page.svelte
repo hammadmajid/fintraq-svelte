3a7627c4e3d874852d5a4189df94640307ed1ce5
Hammad Majid <hammadmajid@proton.me>
2024-08-26 18:04:18 +0500

    create hero section on homepage

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button";
+</script>
+
+<main class="bg-muted/40 flex min-h-screen w-full flex-col">
+  <div class="my-4 md:my-8"></div>
+  <div class="container space-y-4">
+    <div>
+      <h1 class="font-bold text-2xl">Fintraq</h1>
+      <p>A finance tracking app built with SvelteKit and HonoJs.</p>
+    </div>
+
+    <div class="flex flex-row gap-2">
+      <a href="/auth/signup">
+        <Button>Get started</Button>
+      </a>
+      <a href="/auth/signin">
+        <Button variant="outline">Login</Button>
+      </a>
+    </div>
+  </div>
+</main>
