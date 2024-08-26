f7ffb9be73bd5153c7fed4e08dabfc4bae543779
Hammad Majid <hammadmajid@proton.me>
2024-08-26 19:23:58 +0500

    validate form with form action and redirect if success

--- app/src/routes/auth/signup/+page.server.ts
+++ app/src/routes/auth/signup/+page.server.ts
@@ -1,3 +1,5 @@
+import type { Actions } from "./$types";
+import { fail, redirect } from "@sveltejs/kit";
 
 import { superValidate } from "sveltekit-superforms";
 import { zod } from "sveltekit-superforms/adapters";
@@ -8,4 +10,15 @@ export const load = async () => {
   return {
 		form: await superValidate(zod(SignUpForm)),
 	};
+};
+export const actions: Actions = {
+  default: async ({ request }) => {
+    const form = await superValidate(request, zod(SignUpForm));
+
+    if (!form.valid) {
+      return fail(400, { form });
+    }
+
+    return redirect(307, '/app/profile');
+  },
 };
