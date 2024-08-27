54235ed9c56c2f20b6f4e85882630d8f17d10357
Hammad Majid <hammadmajid@proton.me>
2024-08-27 12:53:25 +0500

    use hono logger in middleware

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import { fromHono } from "chanfana";
 import { Hono } from "hono";
 import { csrf } from "hono/csrf";
+import { logger } from 'hono/logger'
 
 import { SignUp } from "endpoints/auth/signup";
 import { SignIn } from "endpoints/auth/signin";
@@ -12,6 +13,7 @@ import { authMiddleware } from "middleware";
 const app = new Hono<{ Bindings: Bindings }>();
 
 // Middleware configuratio
+app.use(logger());
 app.use(csrf());
 app.use("*", authMiddleware);
 
