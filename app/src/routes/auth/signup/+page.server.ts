b3437d0a1823d8b0316579681ef55bbf7b3f0d1b
Hammad Majid <hammadmajid@proton.me>
2024-08-28 17:14:48 +0500

    add logic to sign up a user

--- app/src/routes/auth/signup/+page.server.ts
+++ app/src/routes/auth/signup/+page.server.ts
@@ -1,23 +1,58 @@
-import type { Actions } from "./$types";
+import type { Actions, PageServerLoad } from "./$types";
 import { fail, redirect } from "@sveltejs/kit";
 
-import { superValidate } from "sveltekit-superforms";
+import { message, superValidate } from "sveltekit-superforms";
 import { zod } from "sveltekit-superforms/adapters";
+
+import { API_URL } from "$env/static/private";
+
+// @ts-ignore
+import setCookie from 'set-cookie-parser';
 
 import { SignUpForm } from "@types";
 
-export const load = async () => {
+export const load: PageServerLoad = async ({ locals }) => {
+  if (locals.session) {
+    redirect(302, '/app/profile');
+  }
+
   return {
-		form: await superValidate(zod(SignUpForm)),
-	};
+    form: await superValidate(zod(SignUpForm)),
+  };
 };
 export const actions: Actions = {
-  default: async ({ request }) => {
+  default: async ({ request, cookies }) => {
     const form = await superValidate(request, zod(SignUpForm));
 
     if (!form.valid) {
       return fail(400, { form });
     }
+
+    const data = form.data;
+
+    const response = await fetch(`${API_URL}/auth/signup`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(data)
+    });
+    const parsed = await response.json();
+
+    console.log(parsed);
+    if (response.status !== 200) {
+      return message(form, 'Failed to create an account')
+    }
+
+    const sessionCookie = setCookie.parse(response.headers.getSetCookie()[0])[0];
+
+    cookies.set("session", sessionCookie.value, {
+      path: sessionCookie.path,
+      httpOnly: sessionCookie.httpOnly,
+      sameSite: sessionCookie.sameSite,
+      maxAge: sessionCookie.maxAge,
+      secure: sessionCookie.secure,
+    })
 
     return redirect(307, '/app/profile');
   },
